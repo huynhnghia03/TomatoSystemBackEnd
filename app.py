48385b6d294880e61b76442300cbfab96753088b
LAPTOP-ISTJJHOO\Huynh Huu Nghia <nghiatbag8888@gmail.com>
2024-11-01 11:43:16 +0700

    11_1

--- app.py
+++ app.py
@@ -77,8 +77,6 @@ app.config['MYSQL_USER'] = os.getenv('MYSQL_USER')
 app.config['MYSQL_PASSWORD'] = os.getenv('MYSQL_PASSWORD')
 app.config['MYSQL_DB'] = os.getenv('MYSQL_DB')
 
-
-
 model = my_YoloV8.YOLOv8_ObjectCounter(model_file="best.pt")
 genai.configure(api_key=os.getenv('API_KEY'))
 
